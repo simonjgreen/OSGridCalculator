f78ef9ece338f5520bb83b48029915ce92312a09
Simon Green <simon@sjg.io>
2017-02-25 15:06:38 +0000

    first commit

---
+++ OS Grid Calculator/OS_Grid_Calculator.Droid/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+
+namespace OS_Grid_Calculator.Droid
+{
+	[Activity (Label = "OS Grid Calculator", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+            // Get our text fields from the layout resource,
+            // and attach events to them
+            EditText yourGrid = FindViewById<EditText>(Resource.Id.yourGrid);
+            EditText targetDistance = FindViewById<EditText>(Resource.Id.targetDistance);
+            EditText targetBearing = FindViewById<EditText>(Resource.Id.targetBearing);
+            EditText targetGrid = FindViewById<EditText>(Resource.Id.targetGrid);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.solveButton);
+			button.Click += delegate {
+                int bearing = 0;
+                int distance = 0;
+                bool isInt;
+
+                isInt = int.TryParse(targetBearing.Text, out bearing);
+                if (isInt == false)
+                {
+                    // DisplayAlert("Error", "Invalid bearing entered. Must be a number 0-359", "OK");
+                    return;
+                }
+
+                isInt = int.TryParse(targetDistance.Text, out distance);
+                if (isInt == false)
+                {
+                    // DisplayAlert("Error", "Invalid distance entered. Must be a whole number", "OK");
+                    return;
+                }
+
+                if (OSGridCalculator.osGridValidation.IsMatch(yourGrid.Text) == false)
+                {
+                    return;
+                }
+
+                string result = OSGridCalculator.ComputeTargetGrid(yourGrid.Text, distance, bearing);
+				targetGrid.Text = result;
+			};
+		}
+	}
+}
+
+
---
+++ OS Grid Calculator/OS_Grid_Calculator/OSGridCalculator.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace OS_Grid_Calculator
+{
+    public static class OSGridCalculator
+    {
+        public static string ComputeTargetGrid(string yourGrid, int targetDistance, int targetBearing)
+        {
+            // Which varient on the maths will we need. Derive Theta.
+            string quadrant = null;
+            int angleTheta = 0;
+            if (targetBearing <= 90)
+            {
+                quadrant = "NE";
+                angleTheta = targetBearing;
+            }
+            else if (targetBearing <= 180)
+            {
+                quadrant = "SE";
+                angleTheta = 180 - targetBearing;
+            }
+            else if (targetBearing <= 270)
+            {
+                quadrant = "SW";
+                angleTheta = targetBearing - 180;
+            }
+            else
+            {
+                quadrant = "NW";
+                angleTheta = 360 - targetBearing;
+            }
+
+            //Todo: Explode grid in to component parts
+            int yourEastings = 0;
+            int yourNorthings = 0;
+
+            yourGrid = yourGrid.Replace(" ", "");
+            string yourSquare = yourGrid.Substring(0, 2);
+            string yourCoordsString = yourGrid.Substring(2);
+
+            int gridResolution = yourCoordsString.Length;
+
+            string yourEastingsString = yourCoordsString.Substring(0,gridResolution / 2);
+            string yourNorthingsString = yourCoordsString.Substring(gridResolution / 2);
+
+            int.TryParse(yourEastingsString, out yourEastings);
+            int.TryParse(yourNorthingsString, out yourNorthings);
+            
+            //Math trig functions require a double of radians
+            double angleThetaRads = Math.PI * (double)angleTheta / 180.0;
+
+            //Define adjustment for resolution
+            int adjustment = 1;
+            if (gridResolution == 8)
+            {
+                adjustment = 10;
+            }
+            else if (gridResolution == 6)
+            {
+                adjustment = 100;
+            }
+            else if (gridResolution == 4)
+            {
+                adjustment = 1000;
+            }
+            else if (gridResolution == 2)
+            {
+                adjustment = 10000;
+            }
+
+            //Calculate the target grid
+            int eastingsDelta = (int)(Math.Sin(angleThetaRads) * targetDistance) / adjustment;
+            int northingsDelta = (int)(Math.Cos(angleThetaRads) * targetDistance) / adjustment;
+
+            int targetEastings = 0;
+            int targetNorthings = 0;
+
+            if (quadrant == "NE")
+            {
+                targetEastings = yourEastings + eastingsDelta;
+                targetNorthings = yourNorthings + northingsDelta;
+            }
+            else if (quadrant == "NW")
+            {
+                targetEastings = yourEastings - eastingsDelta;
+                targetNorthings = yourNorthings + northingsDelta;
+            }
+            else if (quadrant == "SE")
+            {
+                targetEastings = yourEastings + eastingsDelta;
+                targetNorthings = yourNorthings - northingsDelta;
+            }
+            else if (quadrant == "SW")
+            {
+                targetEastings = yourEastings - eastingsDelta;
+                targetNorthings = yourNorthings - northingsDelta;
+            }
+
+            string targetSquare = yourSquare;
+
+            //Build grid string and return
+            string targetGrid = targetSquare + " " + targetEastings + " " + targetNorthings;
+            return targetGrid;
+        }
+
+        public static Regex osGridValidation = new Regex(@"^([STNHOstnho][A-Za-z]\s?)(\d{5}\s?\d{5}|\d{4}\s?\d{4}|\d{3}\s?\d{3}|\d{2}\s?\d{2}|\d{1}\s?\d{1})$");
+
+    }
+}
+
